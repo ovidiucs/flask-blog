b91b90f657918238276926ce182825447237b7aa
ovidiucs <ovidiu.ctin@gmail.com>
2015-05-12 21:04:25 +0300

    bootstrap, flask.ext.extension, instance of that main class and initialize, register itself with Flask all the changes are in the template. In template the extends directive, to make the template an extension of another template. Similar to class inheritance, bootstrap base contains all elements that make a bootstrap page.
    We have emmbedded all the constructs in blocks. Define blocks that override or define parts of base template
    Block title we put our content. It's up to the template to decide where the content goes.

--- flask-blog.py
+++ flask-blog.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-
+# initialize extension
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def hello_world():
