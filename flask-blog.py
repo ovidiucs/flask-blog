f6377d8b74ad10d481c30deaa361db365447ed1d
ovidiucs <ovidiu.ctin@gmail.com>
2015-05-12 19:22:26 +0300

    added templates

--- flask-blog.py
+++ flask-blog.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
