3be32e766b1946eeba2dc3aa58da0d5ab9cc6b73
ovidiucs <ovidiu.ctin@gmail.com>
2015-05-14 11:06:56 +0300

    responses

--- flask-blog.py
+++ flask-blog.py
@@ -1,7 +1,8 @@
 import os
 import imghdr
 from datetime import datetime
-from flask import Flask, render_template, request, session, g
+from flask import Flask, render_template, request, session, g, \
+    make_response, jsonify, url_for, redirect
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField, FileField, ValidationError
@@ -34,6 +35,45 @@ class UploadForm(Form):
 def hello_world():
     return render_template('index.html')
 
+
+@app.route('/text')
+def text():
+    return render_template('text.txt'), 200, {'Content-Type': 'text/plain'}
+
+
+@app.route('/xml')
+def xml():
+    return '<h1>this is shown as <b>XML</b> in the browser</h1>', 200, \
+           {'Content-Type': 'application/xml'}
+
+
+@app.route('/json')
+def json():
+    return jsonify({'first_name': 'john', 'last_name': 'Smith'})
+
+
+@app.route('/redirect')
+def redir():
+    return redirect(url_for('text'))
+
+
+@app.route('/cookie')
+def cookie():
+    resp = redirect(url_for('index'))
+    resp.set_cookie('cookie', 'Hello, im a cookie')
+    return resp
+
+
+@app.route('/error')
+def error():
+    return 'Bad Request', 400
+
+
+@app.route('/response')
+def response():
+    resp = make_response(render_template('text.txt'))
+    resp.headers['Content-Type'] = 'text/plain'
+    return resp
 
 @app.route('/user/<name>')
 def user(name):
